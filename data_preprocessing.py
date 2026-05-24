9bf2328c47c0b2f0462ba12d6dce67b5e02e38e2
kushagraa shrimali <126153426+kushagraaashrimali@users.noreply.github.com>
2026-05-24 20:56:40 +0530

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -2,16 +2,12 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from imblearn.over_sampling import SMOTE
 import joblib
-
 # Load dataset
 import pandas as pd
-
 df = pd.read_csv(r"C:\Users\kusha\OneDrive\Desktop\kusha study\demo\creditcard.csv")
 print(df.head())
-
 X = df.drop("Class", axis=1)
 y = df["Class"]
-
 # Train-test split
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, stratify=y, random_state=42
